cff8727bf2567015fac5d4337ea99f98d9f34320
Devin Roche <Roche.devin@ymail.com>
2018-07-27 14:41:46 -0700

    refactor controllers

--- controllers/users.go
+++ controllers/users.go
@@ -1,1 +1,34 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	. "github.com/devinroche/blockcities-server/models"
+)
+
+// get all users
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	db.Find(&users)
+	json.NewEncoder(w).Encode(&users)
+}
+
+// get a user by id
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var user User
+	db.First(&user, params["id"])
+	json.NewEncoder(w).Encode(&user)
+}
+
+// create new user
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	json.NewDecoder(r.Body).Decode(&user)
+	fmt.Println(&user)
+	db.Create(&user)
+	json.NewEncoder(w).Encode(&user)
+}
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -48,29 +47,29 @@ func main() {
 
 }
 
-// get all users
-func GetUsers(w http.ResponseWriter, r *http.Request) {
-	var users []User
-	db.Find(&users)
-	json.NewEncoder(w).Encode(&users)
-}
+// // get all users
+// func GetUsers(w http.ResponseWriter, r *http.Request) {
+// 	var users []User
+// 	db.Find(&users)
+// 	json.NewEncoder(w).Encode(&users)
+// }
 
-// get a user by id
-func GetUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	var user User
-	db.First(&user, params["id"])
-	json.NewEncoder(w).Encode(&user)
-}
+// // get a user by id
+// func GetUser(w http.ResponseWriter, r *http.Request) {
+// 	params := mux.Vars(r)
+// 	var user User
+// 	db.First(&user, params["id"])
+// 	json.NewEncoder(w).Encode(&user)
+// }
 
-// create new user
-func CreateUser(w http.ResponseWriter, r *http.Request) {
-	var user User
-	json.NewDecoder(r.Body).Decode(&user)
-	fmt.Println(&user)
-	db.Create(&user)
-	json.NewEncoder(w).Encode(&user)
-}
+// // create new user
+// func CreateUser(w http.ResponseWriter, r *http.Request) {
+// 	var user User
+// 	json.NewDecoder(r.Body).Decode(&user)
+// 	fmt.Println(&user)
+// 	db.Create(&user)
+// 	json.NewEncoder(w).Encode(&user)
+// }
 
 // get all buildings
 func GetBuildings(w http.ResponseWriter, r *http.Request) {
